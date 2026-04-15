4fd40e2e7cd424627e7cfb30c507e83461d634a8
Izan Rodrigues <contato@izanr.com>
2026-04-15 07:42:14 -0300

    update main.c to test more lib functionality

--- main.c
+++ main.c
@@ -8,6 +8,41 @@ size_t strlen(const char *str) {
   }
 }
 
+char itoa_b[128];
+
+const char *itoa(int num) {
+  int i = 0;
+  bool isNegative = false;
+
+  if (num == 0) {
+    itoa_b[i++] = '0';
+    itoa_b[i] = '\0';
+    return itoa_b;
+  }
+
+  if (num < 0) {
+    isNegative = true;
+    num = -num;
+  }
+
+  while (num != 0) {
+    int rem = num % 10;
+    itoa_b[i++] = (rem > 9) ? (rem - 10) + 'a' : rem + '0';
+    num = num / 10;
+  }
+
+  if (isNegative)
+    itoa_b[i++] = '-';
+
+  itoa_b[i] = '\0';
+
+  for (int j = 0; j < i / 2; j++) {
+    itoa_b[j] = itoa_b[i - j];
+  }
+
+  return itoa_b;
+}
+
 void print(const char *s) { write(1, s, strlen(s)); }
 
 void println(const char *s) {
@@ -16,7 +51,11 @@ void println(const char *s) {
 }
 
 int main(int argc, const char **argv) {
+  println("AAAAA");
+  println(itoa(argc));
+
   for (int i = 0; i < argc; i++) {
     println(argv[i]);
   }
+  return 222;
 }
